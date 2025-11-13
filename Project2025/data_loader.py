472cd0251b684640f68f48616cd0a5f4aeaf934b
S2310053 <S2310053@bi.no>
2025-11-14 00:36:31 +0100

    Some tests to load the data

--- Project2025/data_loader.py
+++ Project2025/data_loader.py
@@ -8,7 +8,7 @@
 
 # Libraries
 import subprocess
-
+import numpy as np
 
 ##########################################
 #
@@ -53,5 +53,26 @@ import subprocess
 
 # Think of this as instance variables from calls that are going to be called to other class
 
-# Get the train dataset mnist_bw
-mnistbwDatasetTrain = subprocess.run(["wget", 'https://www.dropbox.com/scl/fi/fjye8km5530t9981ulrll/mnist_bw.npy?rlkey=ou7nt8t88wx1z38nodjjx6lch&st=5swdpnbr&dl=0'])
+
+# Get the datasets for mnist_bw
+urlTrainBlackWhite     = 'https://www.dropbox.com/scl/fi/fjye8km5530t9981ulrll/mnist_bw.npy?rlkey=ou7nt8t88wx1z38nodjjx6lch&st=5swdpnbr&dl=0'
+urlTestBlackWhite      = 'https://www.dropbox.com/scl/fi/dj8vbkfpf5ey523z6ro43/mnist_bw_te.npy?rlkey=5msedqw3dhv0s8za976qlaoir&st=nmu00cvk&dl=0'
+urlLabelsBlackWhite    = 'https://www.dropbox.com/scl/fi/8kmcsy9otcxg8dbi5cqd4/mnist_bw_y_te.npy?rlkey=atou1x07fnna5sgu6vrrgt9j1&st=m05mfkwb&dl=0'
+
+dataTrainBlackWhite  = subprocess.run(["wget", "-O", "mnist_bw.npy", urlTrainBlackWhite])
+#dataTestBlackWhite   = subprocess.run([command, urlTestBlackWhite])
+#dataLabelsBlackWhite = subprocess.run([command, urlLabelsBlackWhite])
+
+## Transformation mnist_bw/Data preprocessing
+
+# Scalling by making the image interval 0 and 1 (divide data by 255)
+#dataTrainBlackWhite = dataTrainBlackWhite.astype("float32") / 255
+
+# Vectorization: each image is a vector 28*28=784 dimensions
+# Shape of data becomes (60000, 784)
+#dataTrainBlackWhite = dataTrainBlackWhite.reshape((60000, 784))
+
+
+data = np.load("mnist_bw.npy")
+
+

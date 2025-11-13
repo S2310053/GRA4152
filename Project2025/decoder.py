ab3e17c1a7c4b7cc797dd8b83b260d072c847285
S2310053 <S2310053@bi.no>
2025-11-13 20:24:37 +0100

    Finished setting up the decoder class format

--- Project2025/decoder.py
+++ Project2025/decoder.py
@@ -47,4 +47,35 @@ x   = mu + std*eps
 # Output: mu parameter of the Gaussian distribution
 # treat sigma = 0.75 (fixed) as known to make things easier
 # x = mu + std*eps
+#
 #####################################
+
+# Parameters
+target_shape = (4, 4, 128)
+channel_out  = 3
+units        = np.prod(target_shape)
+
+# Convolutional neural network decoder for color MNIST images
+decoder_conv = Sequential(
+                         [
+                         layers.InputLayer(input_shape = (latent_dim, )), #latent_dim from encoder
+                         layers.Dense(units = units, activation = activation), #activation from encoder
+                         layers.Reshape(target_shape = target_shape),
+                         layers.Conv2DTranspose(
+                             filters = 2 * filters, kernel_size = kernel_size, strides = 2, padding = "same", output_padding = 0,
+                             activation = activation), #all object parameters from encoder
+                         layers.Conv2DTranspose(
+                             filters = filters, kernel_size = kernel_size, strides = 2, padding = "same", output_padding = 1,
+                             activation = activation), #all object parameters from encoder
+                         layers.Conv2DTranspose(
+                             filters = channel_out, kernel_size = kernel_size, strides = 2, padding = "same", output_padding = 1),
+                         layers.Activation("linear", dtype = "float32"), #all object parameters are from encoder **channel_out not
+                         ] #look that strides= has a number here and padding= same situation but same values as in encoder
+                         )
+# Output
+mu = decoder_conv(z) #z from encoder
+# We dont' need to learn sigma bc it is challenging
+# Assume a fixed value
+std = 0.75 #just as decoder for bw images
+eps = tf.random.normal(mu.shape)
+x   = mu + std*eps

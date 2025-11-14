15ae0fea6414ca516c9c20ff419a4a41ef51627b
S2310053 <S2310053@bi.no>
2025-11-14 17:37:48 +0100

    loading files from directory

--- Project2025/data_loader.py
+++ Project2025/data_loader.py
@@ -3,8 +3,26 @@
 #  Processes and loads the data
 #
 
-# How to use wget with subprocess.run() link
-# https://www.webscrapingapi.com/effortlessly-download-web-pages-and-files-with-python-and-wget
+# Libraries
+import numpy as np
 
 
-# Create a class that loads the data
+## Load and process mnist_bw data
+#  Needs:
+#  Train  <- mnist_bw.npy
+#  Test   <- mnist_bw_te.npy
+#  Labels <- mnist_bw_y_te.npy
+
+filenameDataTrainBlackWhite = "mnist_bw.npy"
+filenameDataTestBlackWhite  = "mnist_bw_te.npy"
+filenameDataLabelsBlackWhite = "mnist_bw_y_te.npy"
+
+dataTrainBlackWhite  = np.load(filenameDataTrainBlackWhite)
+dataTestBlackWhite   = np.load(filenameDataTestBlackWhite)
+dataLabelsBlackWhite = np.load(filenameDataTestBlackWhite)
+
+## Load (no further processing) mnist_color data
+#  Needs:
+#  Train  <- mnist_color.pkl
+#  Test   <- mnist_color_te.pkl
+#  Labels <- mnist_color_y_te.npy

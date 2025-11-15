b53226cec02d1a32729fa8e7aff699698b3db8b3
S2310053 <S2310053@bi.no>
2025-11-15 10:24:18 +0100

    Formatting to the VAE class

--- Project2025/vae.py
+++ Project2025/vae.py
@@ -6,19 +6,27 @@
 ## Load necessary libraries and packages
 #  @library numpy for vectorization and data handling
 #  @library os, os.environ 3 to just include tensorflow error messages
-#  @library tensorflow let us train our model  
-#  @module tensorflow.keras for MLP encoder and decoder models
-#  
+#  @library tensorflow let us train our model, VAE inherits it to prevent crash 
+#
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = 
 import tensorflow as tf
-from tensorflow.keras import layers
-from tensorflow.keras import activations
-from tensorflow.keras.models import Sequential
 
 ## Variational Autoencoders are models in generative artificial intelligence
 #  They combine deep learning and probabilistic modeling
 #  Which enables generative modelling and representation learning
 #
-class VAE:
+class VAE(tf):
+    ## Train method to update VAE trainable parameters
+    #  @param x our train data set (black and white or colored images)
+    #  @param optimizer adam to improve efficiency in convergence
+    #  @return loss as an accuracy measure of the model
+    #
+    @tf.function
+    def train(self,x, optimizer):
+
+    ## Generates latent space z and visualizes it
+    #  @param posteriorDistribution p
+    #  @return plot of the latent space from posterior distribution
+

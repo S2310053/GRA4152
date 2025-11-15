be9f73f6efe74ba8dd26acf27287c06514c7d1cf
S2310053 <S2310053@bi.no>
2025-11-15 16:53:14 +0100

    Bicoder format is settled

--- Project2025/bicoder.py
+++ Project2025/bicoder.py
@@ -4,11 +4,13 @@
 #  
 
 ## Load necessary libraries and packages
+#  @library numpy as np
 #  @library os, os.environ 3 to just include tensorflow error messages
 #  @module layers from tensorflow.keras reusable when stating weights NN
 #  @module activations from tensorflow.keras adds non-linearity to model
 #  @module Sequential from tensorflow.keras stacks layers
 #
+import numpy as np
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3" 
 from tensorflow.keras import layers
@@ -18,6 +20,43 @@ from tensorflow.keras.models import Sequential
 ## The bicoder captures the direct relationship between encoders and decoders
 #  We know that encoders learn a latent representation z and are used as
 #  input to sample new data and get xhat in the decoder
+#  There's a direct mechanism to get this values
 #
+class BiCoder(layers.Layer):
+    ## Set default parameters that are common in
+    #  encoder and decoders known as class variable
+    #
 
-#class BiCoder(layers.Layer):
+    # Both encoder and decoders for black and white and color images
+    _activation                = "relu"
+   
+    # Encoder black and white images
+    _latentDimensionBlackWhite = 20
+
+    # Encoder color images
+    _filtersColor              = 32
+    _latentDimensionColor      = 50
+    _stridesColor              = 2
+    _kernelSizeColor           = 3
+
+    ## Initialize instance variables in the constructor
+    #
+    def __init__(self):
+
+    ## Computes the z encoder value from a prior p(z) distribution
+    #  @return prior z from and isotropic Gaussian distribution N(0,I)
+    #
+    def encoderPriorDistribution(self):
+
+    ## Computes the z encoder from a posterior q(z|x) distribution
+    #  @param output encoder from the Black and White MLP or Color Convolutional Neural Network
+    #  @param latentDimension either from Black and White or Color images
+    #  @return z as encoder
+    def encoderPosteriorDistribution(self, output, latentDimension):
+
+    ## Computes the xhat decoder where z is used as input
+    #  @param mean of decoder from the Black and White MLP or Color Convolutional Neural Network
+    #  @param standardDeviation fixed as it is challenging
+    #  @return xhat decoder as sample of new data
+    def decoderBlackWhiteColor(self, mean, standardDeviation = 0.75):
+

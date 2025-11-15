27000f075d098fc2b89fce50594e5b0e568620e0
S2310053 <S2310053@bi.no>
2025-11-15 12:06:18 +0100

    Decide which methods are overriden in subclasses

--- Project2025/vae.py
+++ Project2025/vae.py
@@ -7,11 +7,16 @@
 #  @library numpy for vectorization and data handling
 #  @library os, os.environ 3 to just include tensorflow error messages
 #  @library tensorflow let us train our model, VAE inherits it to prevent crash 
-#
+#  @module  TSNE from sklearn.manifold for manifold learning
+#  @module  plt from mathplotlib.pyplot for plotting
+#  @module  ImageGrid form mlp_toolkits.axes_grid1 displays images (multi fixed)
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = 
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3" 
 import tensorflow as tf
+from sklearn.manifold import TSNE
+import mathplotlib.pyplot as plt
+from mlp_toolkits.axes_grid1 import ImageGrid
 
 ## Variational Autoencoders are models in generative artificial intelligence
 #  They combine deep learning and probabilistic modeling
@@ -26,7 +31,15 @@ class VAE(tf):
     @tf.function
     def train(self,x, optimizer):
 
-    ## Generates latent space z and visualizes it
-    #  @param posteriorDistribution p
-    #  @return plot of the latent space from posterior distribution
+    ## Generates and visualizes latent space z
+    #  @param latent space z generated in encoder
+    #  @return scatter plot
+    #
+    #def visualizeLatent(self, visualize = True):
+
+    ## Generates new images
+    #  @ param xhat predicted value generated in decoder
+    #  @ param distribution call the name of distribution
+    #  @ return generated images
+    #def generateImages(self,distribution, xhat):
 

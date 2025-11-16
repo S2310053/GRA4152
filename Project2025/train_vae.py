6d8a49df21d16cfd725fc554c26f468a45573ee9
S2310053 <S2310053@bi.no>
2025-11-16 17:00:45 +0100

    Train model works

--- Project2025/train_vae.py
+++ Project2025/train_vae.py
@@ -1,5 +1,6 @@
 from dataloader import DataLoader
 from vae import VAE
+import tensorflow as tf
 
 loader = DataLoader("mnist_bw")
 train_ds = loader.loadData("mnist_bw")   # (batch, 784)
@@ -9,6 +10,6 @@ optimizer = tf.keras.optimizers.Adam(1e-3)
 
 for epoch in range(10):
     for step, batch in enumerate(train_ds):
-        loss = model.train(batch, optimizer, color=False)
+        loss = model.train_step(batch, optimizer, color=False)
     print(f"Epoch {epoch+1}, loss = {loss.numpy():.4f}")
 

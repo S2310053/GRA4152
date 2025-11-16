c7b17d6ad9e0f1ce506caa6dc83afd5237b525cd
S2310053 <S2310053@bi.no>
2025-11-16 10:21:35 +0100

    Update train_vae.py

--- Project2025/train_vae.py
+++ Project2025/train_vae.py
@@ -1,36 +1,14 @@
-import numpy as np
-import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3" 
-import tensorflow as tf
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-#from mlp_toolkits.axes_grid1 import ImageGrid
-from encoder import Encoder
-from decoder import Decoder
 from dataloader import DataLoader
 from vae import VAE
 
+loader = DataLoader("mnist_bw")
+train_ds = loader.loadData("mnist_bw")   # (batch, 784)
 
-# This PROGRAM represents the pseoudocode for the trian_vae.py
-# 
+model = VAE()
+optimizer = tf.keras.optimizers.Adam(1e-3)
 
-# i) Load the dataset given args arguments
-my_data_loader = DataLoader("mnist_bw")
-
-# ii) Initialize the VAE model
-model = VAE() # Use default values as prof, or pass arguments with argpase
-
-# iii) Set the optimizer
-optimizer = tf.keras.optimizers.Adam(learning_rate = 1e-3)
-
-# iv) Invoke the method train using mini batch from my_data_loader
-tr_data = my_data_loader.loadData("mnist_bw")
-
-# Complete number of epochs pass through entire training set 
-# (one forward pass  + one backward pass)
-# forward take inputs, makes predictions and computes errors
-# backward adjusts weights based on the errors to improve future predictions
-for e in range(20):
-        for i, tr_batch in enumerate(tr_data):
-                    loss = model.train(tr_batch, optimizer)
+for epoch in range(10):
+    for step, batch in enumerate(train_ds):
+        loss = model.train(batch, optimizer, color=False)
+    print(f"Epoch {epoch+1}, loss = {loss.numpy():.4f}")
 
